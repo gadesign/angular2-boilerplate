ceb6a4243a6b225451bd6be44d8b312a3aa96b5c
Jeremy Smith <goodanswerdesign@gmail.com>
2016-04-09 18:19:49 -0600

    Remove product references

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from 'angular2/core';
-import { ProductListComponent } from './products/product-list.component'
+
 @Component({
-    selector:'pm-app',
-    template:`<div><h1>{{ pageTitle }}</h1></div>
-    <pm-products></pm-products>
+    selector:'ng-app',
+    template:`
+    <div><h1>{{ pageTitle }}</h1></div>
     `,
     directives: [ProductListComponent]
 })
+
 export class AppComponent {
     pageTitle: string = 'Good Answer Design';
 }
